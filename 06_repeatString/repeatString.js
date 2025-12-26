bd164e1c89157ee850c7a21a0e2b49338fc36ec3
hinmeru22 <nicesuungsan22@gmail.com>
2025-12-26 23:07:39 +0800

    06_repeatString exercise done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num >= 0) {
+    if (num == 0) {
+      return "";
+    }
+    let joinedString = string;
+    for (let i = 1; i < num; i++) {
+      joinedString += string;
+    }
+    return joinedString;
+  }
+  return "ERROR";
 };
-
 // Do not edit below this line
 module.exports = repeatString;
