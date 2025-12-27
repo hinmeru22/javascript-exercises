ca3d0829481e670c6667b74f7bef4f5f6dc65be3
hinmeru22 <nicesuungsan22@gmail.com>
2025-12-27 15:16:57 +0800

    09_sumAll exercise done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
