6b2e980fb07de2259f170194ef72624e452bda62
hinmeru22 <nicesuungsan22@gmail.com>
2025-12-27 14:22:02 +0800

    08_removeFromArray exercise done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+  const removedArr = arr.filter((element) => !num.includes(element));
+  return removedArr;
 };
 
 // Do not edit below this line
