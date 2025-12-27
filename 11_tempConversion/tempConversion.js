2e94ff38c904cee3263f025558e64c39a92902e6
hinmeru22 <nicesuungsan22@gmail.com>
2025-12-27 19:57:02 +0800

    11_tempConversion exercise done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  const celcius = (degree - 32) * 0.5556;
+  const rounded = celcius.toFixed(1);
+  return Number(rounded);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  const farenheit = degree * 1.8 + 32;
+  const rounded = farenheit.toFixed(1);
+  return Number(rounded);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
