33e9f46e2a2776b2bbf91205e340adb302ad4aa6
hinmeru22 <nicesuungsan22@gmail.com>
2026-01-02 22:24:52 +0800

    14_fibonacci exercise done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let count;
+  if (typeof n !== "number") {
+    count = parseInt(n);
+  } else {
+    count = n;
+  }
 
+  if (count < 0) return "OOPS";
+  if (count == 0) return 0;
+
+  const fib = [0, 1];
+  for (let i = 2; i <= count; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[count];
 };
 
 // Do not edit below this line
